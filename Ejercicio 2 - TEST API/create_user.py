e863b9f8b1edec820ef994bf3c0c1808ab010e19
Asinatum <lucassosa83@gmail.com>
2023-08-31 12:19:44 -0300

    1rst

---
+++ Ejercicio 2 - TEST API/create_user.py
@@ -0,0 +1,19 @@
+import requests
+
+url = 'https://petstore.swagger.io/v2'
+payload = {
+  "id": 25251717,
+  "username": "LucasSosa",
+  "firstName": "Lucas",
+  "lastName": "Sosa",
+  "email": "lucassosa83@gmail.com",
+  "password": "123456123456",
+  "phone": "44556677",
+  "userStatus": 1
+}  #Los datos que quiero enviar. Me los colola dentro del atributo DATA y del atributo JSON.
+
+response = requests.post(url + "/user", json=payload)  #Si enviamos los parametros dentro de json, internamente POST se encarga de serializarlos (los convierte a un json).
+
+print(response.status_code)
+print(response.content)
+
